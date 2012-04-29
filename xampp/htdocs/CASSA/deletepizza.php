33f48e3c3f2022ab793d75e6b2f9049d378fbc1d
qmaseyk <qmaseyk@gmail.com>
2012-04-29 09:48:04 +0000

    QM - Add CSS + revision to files

--- xampp/htdocs/CASSA/deletepizza.php
+++ xampp/htdocs/CASSA/deletepizza.php
@@ -1,62 +1,95 @@
+<?php
+	session_start();									// Start/resume THIS session
+	$_SESSION['title'] = "Delete Pizza | MegaLAN"; 		// Declare this page's Title
+	include("includes/template.php"); 					// Include the template page
+	include("includes/conn.php");						// Inlcude DB connection
+
+
+if (isset($_POST['submit']))
+{
+	$query = "DELETE FROM pizza_type WHERE pizzaID = '".$_POST['pizza_name']."'";
+	$result = $db->query($query) or die(mysql_error());
+}
+?>
+
+
+
 <!-- //******************************************************
 
 // Name of File: deletepizza.php
 // Revision: 1.0
 // Date: 
 // Author: Luke Spartalis
-// Modified:
+// Modified: Quintin Maseyk 29/04/2012
 
 //***********************************************************
 
 //*************** Start of DELETE PIZZA ******************* -->
-<?php
-include("includes/conn.php");
+
+<html>
+<head></head>
+<body>
+<center>
+<div id='shell'>
+
+<!-- Main Content [left] -->
+<div id="content">
 
 
-if (isset($_POST['pizza_name']))
-{
-	$pizza_name = $_POST['pizza_name'];
-	$query2 = "DELETE FROM pizza_type WHERE pizza_name='".$_POST['pizza_name']."'";
-	$result2 = $db->query($query2);
-	$db->close();
-}
-?>
+<div id='deletePizza'>
+	<form name='deletePizza' method='POST' action='deletepizza.php'>
+		
+	<?php 
+		$query = "SELECT * FROM pizza_type";
+		$result = $db->query($query);
+	 
+		echo '<select name="pizza_name">';
+		echo '<option value="-" selected="selected">Please select a Pizza</option>';
 
 
+		for ($i=0; $i<$result->num_rows; $i++) 
+		{
+			$row = $result->fetch_assoc();
+			$pizzaID = $row['pizzaID'];
+			$pizza_name = $row['pizza_name'];
+			
+			// Iterate through and add pizza per line
+			echo '<option value="'.$pizzaID.'">'.$pizza_name.'</option>';
+		}
 
-<?php 
-$query1 = "SELECT pizza_name FROM pizza_type";
+		echo '</select>';
+	?>
 
-$result = $db->query($query1);
-
-for ($i=0; $i<$result->num_rows; $i++) 
-	{
-		$row = $result->fetch_assoc();
-		$pizza_name[] = $row['pizza_name']; // loop to run through result
-	}
-
-
-echo "<form action='deletepizza.php' method='post'>";
-echo "<select name='pizza_name' size='1' id='selpizza_name'>";
-
-
-$option = "<option value=\"Please select a Pizza\">Please select a Pizza</option> \n";
-for ($i = 0; $i < count($pizza_name); $i++) {
-$option .= "<option ";
-$option .= "value=\"$pizza_name[$i]\">$pizza_name[$i]</option> \n";
-
-}
-
-echo $option;
-
-$db->close();
+		<input type="submit" name="submit" value="Delete Pizza" />
+	</form>
+</div>
 
 
 
-?>
-<br /> <input type="submit" />
-</form>
 
-</select>
+
+
+<!-- INCLUDE THIS AFTER 'MAIN CONTENT' -->
+<!-- ********************************* -->
+
+</div><!-- end of: Content -->
+
+
+<!-- INSERT: rightPanel -->
+<?php include('includes/rightPanel.html'); ?>
+
+
+<!-- INSERT: footer -->
+<div id="footer">
+	<?php include('includes/footer.html'); ?>
+</div>
+
+
+</div><!-- end of: Shell -->
+
+</center>
 </body>
 </html>
+
+<!-- ********************************* -->
+<!-- INCLUDE THIS AFTER 'MAIN CONTENT' -->
