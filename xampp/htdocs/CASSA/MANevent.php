2acf75859ba1a6ebf9550e0a07e5a948d512c716
qmaseyk@gmail.com <qmaseyk@gmail.com@fbb12136-791a-4c2d-0b18-bcaf9ada0915>
2012-05-03 07:33:56 +0000

    QM - REVISED

--- xampp/htdocs/CASSA/MANevent.php
+++ xampp/htdocs/CASSA/MANevent.php
@@ -1,22 +1,22 @@
 <?php 
-	session_start();														// Start/resume THIS session
-	$_SESSION['title'] = "Event Management | MegaLAN"; 		// Declare this page's Title
-	include("includes/template.php"); 								// Include the template page
-	include("includes/conn.php"); 					   			// Include the db connection
+	session_start();									// Start/resume THIS session
+	$_SESSION['title'] = "Manage Event | MegaLAN";		// Declare this page's Title
+	include("includes/template.php"); 					// Include the template page
+	include("includes/conn.php"); 						// Include the DB Connection
 ?>
 
 
 <!-- //******************************************************
 
-// Name of File: eventManagement.php
+// Name of File: MANevent.php
 // Revision: 1.0
-// Date: 16/04/2012
-// Author: Lyndon Smith
+// Date: 29/04/2012
+// Author: Quintin M
 // Modified: 
 
 //***********************************************************
 
-//********** Start of MANAGE CONTACTS PAGE ************** -->
+//************* Start of MANAGE EVENT PAGE ************** -->
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -29,45 +29,19 @@
 
 <!-- Main Content [left] -->
 <div id="content">
-	<h1>Event Management</h1>
-	
-
-<?php
-//***********************************************
-// Set some variables up for use
-//***********************************************
-
-$username = $_SESSION['username'];
-	$query = "SELECT * FROM event";
-	$result = $db->query($query);
-
-echo '<hr />';
-echo '<p><h2>Current Events</h2></p>';
-echo '<FORM action="selectEvent.php method="post">';
-echo '<P>';
-echo '<SELECT size="4" name="selectEvent">';
-// Now we can output the option fields to poulate the list box.
-for ($i = 0; $i<$result->num_rows;$i++) 
-	{
-		$row = $result->fetch_array(MYSQLI_BOTH);    
-    echo '<OPTION selected value="' . $row[0]. '">' . $row[1] . '</OPTION>';
-	}
-
-		echo '</SELECT>';
-			echo '<br />';
-   		echo '<INPUT type="submit" value="Send"><INPUT type="reset">';
-   			echo '</P>';
-					echo '</FORM>';
-						echo '<hr />';
-
-
-?>
+	<h1>Manage Event</h1>
 
 
 
-    
-      
-   
+
+
+
+
+
+
+
+
+
 
 
 
