16f83c4655385da1463098d42c9495aa66e0a3b4
qmaseyk <qmaseyk@gmail.com>
2012-05-03 07:33:56 +0000

    QM - REVISED

--- xampp/htdocs/CASSA/MANpizza.php
+++ xampp/htdocs/CASSA/MANpizza.php
@@ -1,26 +1,59 @@
+<?php 
+	session_start();
+	//$_SESSION['pizzaID'];
+	//$_SESSION['prevPage'];
+	
+
+	print_r($_POST);
+?>
+
 <!-- //******************************************************
 
 // Name of File: menumanagement.php
 // Revision: 1.0
 // Date: 
 // Author: Luke Spartalis
-// Modified:
+// Modified: Quintin Maseyk 03/05/2012
 
 //***********************************************************
 
 //*************** Start of CREATE PIZZA ******************* -->
 
-
-
 <html>
+<head>
+
+<script type='text/javascript'>
+
+function editRow(x, totalRows)
+{
+	// DISPLAY [this] ROW FROM TEXT -> EDITABLE
+	document.getElementById(x+"_normal").style.display = 'none';
+	document.getElementById(x+"_edit").style.display = 'block';
+}
+function saveRow(x)
+{
+	var answer = confirm("Please confirm new details");
+	if (answer == true)
+	{
+		// SEND [this] FORM TO SERVER
+		document.forms[x+"_form"].submit();
+	}
+}
+</script>
+
+</head>
 <body>
 
-<?php 
-	session_start();
-	//$_SESSION['pizzaID'];
-	//$_SESSION['prevPage'];
-	
-?>
+
+<table rules='rows' cellpadding='5px'>
+<caption>Pizza's</caption>
+<th>
+	<td>ID</td>
+	<td>Pizza Name</td>
+	<td>Description</td>
+	<td>Price</td>
+</th>
+
 
 <?php
 if (isset($_POST['pizzaID'])) // Delete pizza from menu
@@ -41,35 +74,84 @@ if (isset($_POST['pizzaID'])) // Delete pizza from menu
 	$query = "SELECT * FROM pizza_type";
 	$result = $db->query($query);
 
-echo "<center> Pizzas </center>";
+
+/*for ($i=0; $i<$result->num_rows; $i++) // create a list of all pizza's in the database
+{
+	$row = $result->fetch_assoc();
+
+	echo "</ br> <img src='images/buttons/add_60.png' alt='Add'/>" . " ";
+	echo "<img src='images/buttons/edit_60.png' alt='Edit'/> </a>" . " " ;
+	echo "<b>Pizza ID:</b> " . $pizzaID[] = $row['pizzaID'] . ", ";
+	echo "<b>Pizza Name:</b> " . $pizza_name[] = $row['pizza_name'] . ", ";
+	echo "<b>Description:</b> " . $description[] = $row['description'] . ", ";
+	echo "<b>Price:</b> " . $price[] = $row['price'] . ", ";
+	echo "<br /><br />";
+}*/
+
+
 
 for ($i=0; $i<$result->num_rows; $i++) // create a list of all pizza's in the database
-  {
-$row = $result->fetch_assoc();
-  echo "</ br> <img src='images/buttons/add_60.png' alt='Add'/>" . " ";
-  echo "<img src='images/buttons/edit_60.png' alt='Edit'/> </a>" . " " ;
-  echo "<b>Pizza ID:</b> " . $pizzaID[] = $row['pizzaID'] . ", ";
-  echo "<b>Pizza Name:</b> " . $pizza_name[] = $row['pizza_name'] . ", ";
-  echo "<b>Description:</b> " . $description[] = $row['description'] . ", ";
-  echo "<b>Price:</b> " . $price[] = $row['price'] . ", ";
-  echo "<br /><br />";
-  }
+{
+	$row = $result->fetch_assoc();
 
- ?>
+	// [this] NORMAL ROW
+	echo "<tr id='".$i."_normal'>";
+
+		echo "<td>";
+			echo "<img class='pointer' src='images/buttons/add_60.png' alt='Add' />";
+			echo "<img class='pointer' src='images/buttons/edit_60.png' alt='Edit' onclick='editRow(".$i.", ".$result->num_rows.")' />";
+		echo "</td>";
+
+		echo "<td>" . $row['pizzaID'] . "</td>";
+
+		echo "<td>" . $row['pizza_name'] . "</td>";
+
+		echo "<td>" . $row['description'] . "</td>";
+
+		echo "<td>" . $row['price'] . "</td>";
+
+	echo "</tr>";
+
+	// [this] EDITABLE ROW
+	// CREATE FORM FOR SUBMISSION
+	echo "<form name='".$i."_form' method='POST' action='MANpizza.php' >";
+	echo "<tr id='".$i."_edit' style='display: none;'>";
+		
+		echo "<td>";
+			echo "<img src='images/buttons/add_60.png' alt='Add' />";
+			echo "&nbsp;&nbsp;&nbsp;<img src='images/layers/tick.png' alt='Save' onclick='saveRow(".$i.")' />";
+		echo "</td>";
+
+		echo "<td>" . $row['pizzaID'] . "</td>";
+
+		echo "<td><input type='text' name='pizza_name' id='pizza_name' value='".$row['pizza_name']."' /></td>";
+		
+		echo "<td><input type='text' name='description' id='description' value='".$row['description']."' /></td>";
+
+		echo "<td><input type='text' name='price' id='price' value='".$row['price']."' size='5' maxlength='5' /></td>";
+	echo "</tr>";
+	echo "</form>";
+}
+
+
+
+
+?>
   
 
   <?php
-  echo "<center>Current Pizza Menu </center>";
+	echo "<center>Current Pizza Menu </center>";
 
-  $query1 = "SELECT menu_items.pizzaID, pizza_type.pizza_name FROM menu_items INNER JOIN pizza_type ON menu_items.pizzaID = pizza_type.pizzaID WHERE menuID ='1'";
-  $result1 = $db->query($query1);
+	$query1 = "SELECT menu_items.pizzaID, pizza_type.pizza_name FROM menu_items INNER JOIN pizza_type ON menu_items.pizzaID = pizza_type.pizzaID WHERE menuID = '1'";
+	$result1 = $db->query($query1);
+	
 	for ($i=0; $i<$result1->num_rows; $i++) //create a list of pizza's currently on menu
 	{
-	$row1 = $result1->fetch_assoc();
-	  echo  "<head> <img src='images/buttons/delete_60.png' alt='Delete' onclick='del(document.getElementById('rowID').value)' /> </head>";
-	  echo	" " . $pizzaID[] = $row1['pizzaID'] ;
-	  echo	" " . $pizza_name[] = $row1['pizza_name'] . "<br />";
+		$row1 = $result1->fetch_assoc();
 
+		echo  "<head> <img src='images/buttons/delete_60.png' alt='Delete' onclick='del(document.getElementById('rowID').value)' /> </head>";
+		echo	" " . $pizzaID[] = $row1['pizzaID'] ;
+		echo	" " . $pizza_name[] = $row1['pizza_name'] . "<br />";
 	}
 
 	$db->close();
--- xampp/htdocs/CASSA/seatMap.php
+++ xampp/htdocs/CASSA/seatMap.php
@@ -4,6 +4,12 @@
 	include("includes/template.php"); 					// Include the template page
 	include("includes/conn.php"); 					// Include the database con
 
+
+	// IF SOMEONE CLICKED TO BOOK A SEAT
+	if (isset($_POST['seatID']))
+	{
+		
+	}
 ?>
 
 
@@ -24,19 +30,37 @@
 <head>
 
 <script type='text/javascript'>
-	function showName(number)
+	function showName(seat)
 	{
 		// SEAT NUMBER
-		document.getElementById('seatNumber').value = number;
+		document.getElementById('seatNumber').value = seat;
 
 		// SEAT NAME
-		var name = document.getElementById(number + 'name').value;
+		var name = document.getElementById(seat + 'name').value;
 		document.getElementById('seatName').value = name;
 	}
 	function hideName()
 	{
-		document.getElementById('seatN').value = '';
+		document.getElementById('seatNumber').value = '';
 		document.getElementById('seatName').value = '';
+	}
+	function bookSeat(seat)
+	{
+		if (document.getElementById(seat + 'name').value.length <= 2)
+		{
+			document.getElementById('seatReady').value = 'YES';
+			var answer = confirm("Please confirm to book seat number "+seat);
+
+			if (answer == true)
+			{
+				document.bookThisSeat['seatID'].value = seat;
+				document.bookThisSeat.submit();
+			}
+		}
+		else
+		{
+			document.getElementById('seatReady').value = 'NO';
+		}
 	}
 </script>
 
@@ -51,24 +75,57 @@
 <div id="content">
 	<h1>Seat Availability</h1>
 
+
+
+<!-- FORM IN WHICH GETS POSTED IF A USER CLICKS TO BOOK A SEAT -->
+<form name='bookThisSeat' method='POST' action='seatMap.php'>
+	<input type='hidden' name='seatID' id='seatID' value='' />
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- SMALL LAYOUT IMAGE 
+	 onclick ENLARGE IMAGE WITH EXTRA DETAILS -->
 <a href="javascript:void(0)" onclick="document.getElementById('light').style.display='block';document.getElementById('fade').style.display='block'">
-		<img src='images/seatPlan/layout.png' border='0' width='571px' height='239px' />
+	<!-- SMALL IMAGE -->
+	<img src='images/seatPlan/layout.png' border='0' width='571px' height='239px' />
 </a>
 
+
+<!-- LARGE LAYOUT IMAGE -->
 <div id="light" class="white_content">
+
+	<!-- CLOSE BUTTON -->
 	<div id="closeButton">
-		<a href="javascript:void(0)" onclick="document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'"><img src='images/layers/cross.png' width='20px' height='20px' border='0' /></a>
-	</div>
+		<a href="javascript:void(0)" onclick="document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">
+		
+		<img src='images/layers/cross.png' width='20px' height='20px' border='0' /></a>
+	</div><!-- end of: CLOSE BUTTON -->
 
+
+	<!-- SEAT PLAN LAYOUT -->
 	<img src='images/seatPlan/layout.png' border='0' />
-	
 
+	
+	<!-- BOTTOM RIGHT LEGEND -->
 	<div id='legend'>
 		<img src='images/seatPlan/seatTop_Y30.png' /> <b>Available</b> 
 		<img src='images/seatPlan/seatTop_N30.png' /> <b>Booked</b>
 		<img src='images/seatPlan/seatTop_R30.png' /> <b>Reserved</b>
-	</div>
+	</div><!-- end of: LEGEND -->
 
+
+	<!-- BOTTOM SEAT DETAIL -->
 	<div id='seatDetails'>
 	Seat Number: 
 		<input type='text' name='seatNumber' id='seatNumber' value='' size='2' readonly='readonly' />
@@ -77,7 +134,12 @@
 	
 	Reserved For: 
 		<input type='text' name='seatName' id='seatName' value='' size='32' readonly='readonly' />
-	</div>
+
+	&nbsp;&nbsp;&nbsp;
+	
+	Good to book?: 
+		<input type='text' name='seatReady' id='seatReady' value='' size='3' readonly='readonly' />
+	</div><!-- end of: SEAT DETAIL -->
 
 
 	<!-- GET CURRENT SEAT STATUS -->
@@ -134,6 +196,7 @@
 		// SET MOUSE EVENTS
 		$mover = " onmouseover='showName(this.id)'";
 		$mout = " onmouseout='hideName()'";
+		$onclick = " onclick='bookSeat(this.id)'";
 	?>
 
 
@@ -147,7 +210,7 @@
 			echo "<input type='hidden' name='".($i+1)."name' id='".($i+1)."name' value='".$client[$i]."' />";
 
 				// [this] SEAT 
-				echo "<td id='".($i+1)."' ".$mover.$mout.">";
+				echo "<td id='".($i+1)."' ".$mover.$mout.$onclick.">";
 					
 					// [this] SEAT NUMBER
 					echo "<div class='seatNumberTop'>".($i+1)."</div>";
@@ -167,7 +230,7 @@
 			echo "<input type='hidden' name='".($i+1)."name' id='".($i+1)."name' value='".$client[$i]."' />";
 
 				// [this] SEAT 
-				echo "<td id='".($i+1)."' ".$mover.$mout.">";
+				echo "<td id='".($i+1)."' ".$mover.$mout.$onclick.">";
 
 					// [this] SEAT NUMBER
 					echo "<img class='seat_sm pointer' src='".$src.$bot.$seatStatus[$i].$ext;
@@ -266,7 +329,24 @@
 
 
 </div>
+<!-- BLACK BACKGROUND -->
 <div id="fade" class="black_overlay"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
