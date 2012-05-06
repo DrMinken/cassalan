89ab06e534611ec063e91df9afc237aecfbd3d00
nashamg <nashamg@gmail.com>
2012-05-06 23:24:11 +0000

    customer registration code

---
+++ xampp/htdocs/CASSA/registrationA.php
@@ -0,0 +1,78 @@
+<!-- //******************************************************
+
+// Name of File: index.php
+// Revision: 1.0
+// Date: 30/04/2012
+// Author: Tinashe
+// Modified: 5/05/2012
+
+//************************************************************ -->
+<html>
+<h1>Registration</h1>
+
+<form action ='registrationB.php' method ='POST'>
+    <table>
+    <tr>
+    <td>
+Choose a username:
+    </td>
+    <td>
+    <input type='text' name='username'>
+    </td>
+    </tr>
+    <tr>
+    <td>
+Enter a password:
+    </td>
+    <td>
+    <input type='password' name='password'>
+    </td>
+    </tr>
+    <tr>
+    <td>
+Repeat Your Password:
+    </td>
+    <td>
+    <input type='password' name='repeatpassword'>
+    </td>
+    </tr>
+    <tr>
+    <td>
+Enter Your First Name:
+    </td>
+    <td>
+    <input type='text' name='first_name'>
+    </td>
+    </tr>
+    <tr>
+    <td>	
+Enter Your Last Name:	
+    </td>
+    <td>
+    <input type='text' name='last_name'>
+    </td>
+    </tr>
+    <tr>
+    <td>
+Enter Your Mobile Number:
+    </td>
+    <td>
+    <input type='text' name='mobile'>
+    </td>
+    </tr>
+    <tr>
+    <td>
+Enter Your Email:
+    </td>
+    <td>
+    <input type='text' name='email'>
+    </td>
+    </tr>
+    </table>
+<p>
+<input type='submit' name='submit' value='Register'>
+
+
+</form>
+
+</html>
