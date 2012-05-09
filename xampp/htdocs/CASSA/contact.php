b9e328b6e19f5ead51637fc59aee17124c8bb94d
qmaseyk@gmail.com <qmaseyk@gmail.com@fbb12136-791a-4c2d-0b18-bcaf9ada0915>
2012-05-09 00:41:58 +0000

    QM - Updated contact.php

--- xampp/htdocs/CASSA/contact.php
+++ xampp/htdocs/CASSA/contact.php
@@ -28,17 +28,22 @@
 
 <!-- Main Content [left] -->
 <div id="content">
-	<h1>MegaLAN Contact Details</h1>
+	<h1>Contact</h1>
 
 
-<h3>Clubroom</h3>
+
+<br />
+
+
+
+<h3 style='line-height: 20pt;'>Clubroom</h3>
 
 <div style='height: 146px'>
 	<div style='float: left'>
 		<img src='images/layers/clubroom.jpg' width='120px' height='146px' />
 		<br />
 	</div>
-	<div style='float: right; margin-left: 20px; word-spacing: 2px;'>
+	<div style='float: right; margin-left: 15px; word-spacing: 2px;'>
 		We are located on ECU Mt. Lawley campus, room 03.202 (upstairs next to the lecture theatre). We have the screen on top of our door so you won’t miss us.<br /><br />
 		 
 		We aim to be open most of the day during university hours. You can buy drinks and candy from us here.
@@ -46,6 +51,9 @@
 	</div>
 </div>
 
+
+
+<br />
 
 
 
