506653f91cd168baebcdae4ee5faed8ed31fe4ca
qmaseyk <qmaseyk@gmail.com>
2012-05-09 09:02:46 +0000

    QM - Updated StaffBoard page

--- xampp/htdocs/CASSA/management/staffBoard.php
+++ xampp/htdocs/CASSA/management/staffBoard.php
@@ -38,61 +38,63 @@
 
 
 
-
-
-<div class='boardDIV'>
-	<table cellspacing='30px'>
-	<tr>
-		<td>
-			<!-- MANAGE EVENT -->
-			<a href='MANevent.php'>
-				<img class='boardButton' src='../images/buttons/event.jpg' title='Manage Event' 
-					onmouseover='this.src="../images/buttons/eventO.jpg"' 
-					onmouseout='this.src="../images/buttons/event.jpg"' />
-			</a>
-		</td>
-		<td>
-			<!-- MANAGE TOURNAMENT -->
-			<a href='MANtournament.php'>
-				<img class='boardButton' src='../images/buttons/tournament.jpg' title='Manage Tournaments'
-					onmouseover='this.src="../images/buttons/tournamentO.jpg"' 
-					onmouseout='this.src="../images/buttons/tournament.jpg"' />
-			</a>
-		</td>
-		<td>
-			<!-- MANAGE PIZZA MENU -->
-			<a href='MANpizza.php'>
-				<img class='boardButton' src='../images/buttons/pizza.jpg' title='Manage Pizza'
-					onmouseover='this.src="../images/buttons/pizzaO.jpg"' 					onmouseout='this.src="../images/buttons/pizza.jpg"' />
-			</a>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<!-- MANAGE USERS -->
-			<a href='MANuser.php'>
-				<img class='boardButton' src='../images/buttons/user.jpg' title='Manage Users' 
-					onmouseover='this.src="../images/buttons/userO.jpg"' 
-					onmouseout='this.src="../images/buttons/user.jpg"' />
-			</a>
-		</td>
-		<td>
-			<!-- MANAGE PAGES -->
-			<a href='MANpages.php'>
-				<img class='boardButton' src='../images/buttons/page.jpg' title='Manage Pages'
-					onmouseover='this.src="../images/buttons/pageO.jpg"' 
-					onmouseout='this.src="../images/buttons/page.jpg"' />
-			</a>
-		</td>
-		<td>
-			<!-- SETTINGS -->
-			<a href='settings.php'>
-				<img class='boardButton' src='../images/buttons/settings.jpg' title='Site Settings'
-					onmouseover='this.src="../images/buttons/settingsO.jpg"' 					onmouseout='this.src="../images/buttons/settings.jpg"' />
-			</a>
-		</td>
-	</tr>	</table>
-</div>
+<table class='boardDIV' align='center' border='0'>
+<tr>
+	<td width='100px;'>&nbsp;</td>
+	<td>
+		<!-- MANAGE EVENT -->
+		<a href='MANevent.php'>
+			<img class='boardButton' src='../images/buttons/event.jpg' title='Manage Event' 
+				onmouseover='this.src="../images/buttons/eventO.jpg"' 
+				onmouseout='this.src="../images/buttons/event.jpg"' />
+		</a>
+	</td>
+	<td>
+		<!-- MANAGE TOURNAMENT -->
+		<a href='MANtournament.php'>
+			<img class='boardButton' src='../images/buttons/tournament.jpg' title='Manage Tournaments'
+				onmouseover='this.src="../images/buttons/tournamentO.jpg"' 
+				onmouseout='this.src="../images/buttons/tournament.jpg"' />
+		</a>
+	</td>
+	<td>
+		<!-- MANAGE PIZZA MENU -->
+		<a href='MANpizza.php'>
+			<img class='boardButton' src='../images/buttons/pizza.jpg' title='Manage Pizza'
+				onmouseover='this.src="../images/buttons/pizzaO.jpg"' 					onmouseout='this.src="../images/buttons/pizza.jpg"' />
+		</a>
+	</td>
+	<td width='100px;'>&nbsp;</td>
+</tr>
+<tr>
+	<td width='100px;'>&nbsp;</td>
+	<td>
+		<!-- MANAGE USERS -->
+		<a href='MANuser.php'>
+			<img class='boardButton' src='../images/buttons/user.jpg' title='Manage Users' 
+				onmouseover='this.src="../images/buttons/userO.jpg"' 
+				onmouseout='this.src="../images/buttons/user.jpg"' />
+		</a>
+	</td>
+	<td>
+		<!-- MANAGE PAGES -->
+		<a href='MANpages.php'>
+			<img class='boardButton' src='../images/buttons/page.jpg' title='Manage Pages'
+				onmouseover='this.src="../images/buttons/pageO.jpg"' 
+				onmouseout='this.src="../images/buttons/page.jpg"' />
+		</a>
+	</td>
+	<td>
+		<!-- SETTINGS -->
+		<a href='settings.php'>
+			<img class='boardButton' src='../images/buttons/settings.jpg' title='Site Settings'
+				onmouseover='this.src="../images/buttons/settingsO.jpg"' 					onmouseout='this.src="../images/buttons/settings.jpg"' />
+		</a>
+	</td>
+	<td width='100px;'>&nbsp;</td>
+</tr>	
+<tr><td colspan='5' height='40px'>&nbsp</td></tr>
+</table>
 
 
 
