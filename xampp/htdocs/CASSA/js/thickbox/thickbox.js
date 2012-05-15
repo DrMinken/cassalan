7ee4f053c2456dd105f771f2a015c238712694c4
qmaseyk@gmail.com <qmaseyk@gmail.com@fbb12136-791a-4c2d-0b18-bcaf9ada0915>
2012-05-15 11:18:18 +0000

    QM - Thickbox FIX

--- xampp/htdocs/CASSA/js/thickbox/thickbox.js
+++ xampp/htdocs/CASSA/js/thickbox/thickbox.js
@@ -5,8 +5,7 @@
  * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
 */
 		  
-var tb_pathToImage = "../js/thickbox/loadingAnimation.gif";
-
+var tb_pathToImage = "/cassa/js/thickbox/loadingAnimation.gif";
 
 /*!!!!!!!!!!!!!!!!! edit below this line at your own risk !!!!!!!!!!!!!!!!!!!!!!!*/
 
